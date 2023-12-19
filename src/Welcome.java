686a2cdb4f11fa03c3329570a9d2eec7299bae25
Shrishti Shah <shubhasritadepalli@gmail.com>
2023-12-19 23:17:01 +0530

    Welcome

---
+++ src/Welcome.java
@@ -0,0 +1,5 @@
+public class Welcome {
+    public static void main(String[] args) throws Exception {
+        System.out.println("................Welcome to Line Comparison Problem..................");
+    }
+}
